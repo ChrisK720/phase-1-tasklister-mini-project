d20e6245f70b76e396db29a4eb12b387686c86f9
Christopher Kirathe <chrisk720@Christophers-MacBook-Pro.local>
2024-01-15 08:20:02 +0300

    done with all deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// Type task in input field
+
+// THINGS NEEDED:
+// id of form = "create-task-form"
+// id of field = "new-task-description"
+
+// STEPS:
+// add an eve lis to from => event should be 'submit' (MAKE SURE e.preventDef())
+// This covers first 2 tasks
+
+// Callback function for submit event:
+ //  Function should:
+ // total => see task appear after sumission
+// STEPS:
+// 1) e.preventDef()
+// 2) get the value of fied where input was made. con In = document.getElementById("new-task-description").value
+// 3) define ul and appened a li with In as it's text content
+// 3:
+// con ul = document.getElementById("tasks")
+// con newItem = document.createElement('li')
+// new newItem.textContent = In
+// ul.appendChild(newItem)
+
+// This should complete project only Fetch lab remaining
+
+function mini (){
+     const form = document.getElementById('create-task-form')
+     const field = document.getElementById('new-task-description')
+     form.addEventListener('submit', (e) => {
+          e.preventDefault()
+          const inp = field.value
+          const ul = document.getElementById('tasks')
+          const listItem = document.createElement('li')
+          listItem.textContent = inp
+          ul.appendChild(listItem)
+
+     })
+}
+
+mini()
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//      document.querySelector("form").addEventListener("submit", (e) =>{
+//      e.preventDefault()
+//      const p = document.createElement('p')
+//           p.id = 'paratas'
+//           document.getElementById('paratas').addEventListener()
+//           document.getElementById('paratas').append(task)
+     
+ 
+//      })
+// })
